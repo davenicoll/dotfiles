e885cab8eb715314c8d7e641fca1689d830d4bbf
Dave Nicoll <dnicoll@firstam.com>
2021-09-07 14:13:49 -0700

    More tweaks

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-dotfiles-ubuntu
+# Personal dotfiles and scripts
+
+Pilfer at your own peril :)
+
+## Installation
+
+**Warning:** If you want to give these dotfiles a try, you should first fork this repository, review the code, and remove things you don’t want or need. Don’t blindly use my settings unless you know what that entails. 
+
+``` shell
+git clone https://github.com/davenicoll/dotfiles-ubuntu.git
+cd dotfiles-ubuntu
+./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 28
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
-gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
-gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style SEGMENTED
-gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button false
-gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner true
-gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color true
-
-gsettings set org.gnome.shell.extensions.ding show-trash false
-#gsettings set org.gnome.shell.extensions.ding show-home false
-#gsettings set org.gnome.shell.extensions.ding show-volumes false
-gsettings set org.gtk.Settings.FileChooser show-hidden true
-
 sudo apt update && sudo apt upgrade -y
 
 # standard toolchain
@@ -52,5 +35,29 @@ sudo apt-get update
 sudo add-apt-repository universe
 sudo apt-get install -y powershell
 
+# Gnome tweaks
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.interface font-name 'SF Pro 11'
 gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 11'
+
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style SEGMENTED
+gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button false
+gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner true
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color true
+
+gsettings set org.gnome.shell.extensions.ding show-trash false
+#gsettings set org.gnome.shell.extensions.ding show-home false
+#gsettings set org.gnome.shell.extensions.ding show-volumes false
+gsettings set org.gtk.Settings.FileChooser show-hidden true
+
+#ln -sf "$PWD/.bashrc" "$HOME/.bashrc"
+#ln -sf "$PWD/.dircolors" "$HOME/.dircolors"
+#ln -sf "$PWD/.inputrc" "$HOME/.inputrc"
+#ln -sf "$PWD/.profile" "$HOME/.profile"
