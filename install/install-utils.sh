b2e97e0640c519b27c220cabc49419624d6ad7a7
Dave Nicoll <davenicoll@users.noreply.github.com>
2021-09-07 21:05:41 -0700

    another

--- install/install-utils.sh
+++ install/install-utils.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-sudo apt install curl wget exa git neofetch xclip shellcheck p7zip-full tilix build-essential silversearcher-ag fzf jq ffmpeg flameshot gnome-shell-extension-prefs -y
+sudo apt install curl figlet lolcat wget exa git neofetch xclip shellcheck p7zip-full tilix build-essential silversearcher-ag fzf jq ffmpeg flameshot gnome-shell-extension-prefs -y
+
+# configure print screen to trigger flameshot
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Flameshot'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'Print'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'flameshot gui'"
+
+# configure flameshot settings
+if ! test -d "$HOME/.config/flameshot/"; then mkdir $HOME/.config/flameshot; fi
+if test -f "$HOME/.config/flameshot/flameshot.ini"; then rm "$HOME/.config/flameshot/flameshot.ini"; fi
+tee -a $HOME/.config/flameshot/flameshot.ini << EOL
+[General]
+contrastUiColor=#bf1c00
+disabledTrayIcon=false
+drawColor=#ff0000
+drawThickness=0
+saveAfterCopyPath=$HOME/Pictures
+showHelp=false
+startupLaunch=true
+uiColor=#ff5100
+EOL
--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Ask for auth...
+sudo -v
+
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 sudo apt update && sudo apt upgrade -y
 
 # Utils (needs to run before the other scripts, installs curl, wget, etc)
 source ./install/install-utils.sh
+
+exit
 
 # Cloud tools
 source ./install/install-awscli.sh
