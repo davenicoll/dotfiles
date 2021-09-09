a86c51922758bf0e3b2c0d8d50570862c3774109
DN <690117+davenicoll@users.noreply.github.com>
2021-09-08 21:33:26 -0700

    Download vscode silently

--- install/install-vscode.sh
+++ install/install-vscode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $(which code | grep 'bin' | wc -l) -eq 0 ]; then
-    curl -o vscode.deb -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+    curl --silent -o vscode.deb -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
     sudo dpkg -i vscode.deb
     rm vscode.deb
 else
