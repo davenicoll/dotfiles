69b207c93014c6971917137d0dd3a7a92710cc93
DN <690117+davenicoll@users.noreply.github.com>
2021-09-08 21:32:09 -0700

    Make apt update quietly

--- install/install-az.sh
+++ install/install-az.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ $(az --version 2>/dev/null | grep 'azure-cli' | wc -l) -eq 0 ]; then
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg -y
+    sudo apt-get update -qq
+    sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg -y -qq
     curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
     AZ_REPO=$(lsb_release -cs)
     echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-    sudo apt-get update
-    sudo apt-get install azure-cli -y
+    sudo apt-get update -qq
+    sudo apt-get install azure-cli -y -qq
 else
     echo "az already installed"
     az --version | head -n 1
