55dd886f0db61fa9cf8699f09a976160014b6f51
Dave Nicoll <dnicoll@firstam.com>
2021-09-09 13:09:06 -0700

    Docker installer improvement

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -2,10 +2,16 @@
 
 if [ $(which docker | grep 'bin' | wc -l) -eq 0 ]; then
     # use docker convenience installer
+    sudo apt install uidmap --q -y
     curl -fsSL https://get.docker.com -o get-docker.sh
     chmod +x ./get-docker.sh
     sudo sh ./get-docker.sh
     rm ./get-docker.sh
+    
+    # Setup docker for rootless use
+    dockerd-rootless-setuptool.sh install
+    sudo usermod -aG docker ${USER}
+    sudo systemctl start docker
 else
     echo "docker already installed"
     docker version
