b115cf2350e502f24d6a66ade70f5ae46910f800
David Nicoll <690117+davenicoll@users.noreply.github.com>
2021-10-24 21:38:00 -0700

    python (and pyenv) added
    pyenv switch to newly installed python version
    
    
    Fix for missing shim path
    
    
    Added missing library for python _ctypes
    
    
    Added dependencies

--- install/install-utils.sh
+++ install/install-utils.sh
@@ -4,4 +4,4 @@
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 echo ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note | sudo debconf-set-selections
 
-sudo apt install curl figlet lolcat wget exa git neofetch xclip nmap htop shellcheck obs-studio p7zip-full unzip file build-essential silversearcher-ag fzf jq ffmpeg gnome-shell-extension-prefs ttf-mscorefonts-installer -y -qq
+sudo apt install nano libssl-dev curl figlet lolcat wget exa git neofetch xclip nmap htop shellcheck obs-studio p7zip-full unzip file build-essential silversearcher-ag fzf jq ffmpeg gnome-shell-extension-prefs ttf-mscorefonts-installer zlib1g-dev libreadline-dev libreadline8 sqlite3 libsqlite3-dev libbz2-dev libffi-devel -y -qq
