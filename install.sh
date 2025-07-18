15b98b625cb8c25fc5512af301b212ccef935944
Dave Nicoll <690117+davenicoll@users.noreply.github.com>
2025-07-18 13:11:57 -0700

    added missing btop

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ if [[ $(which brew | grep bin | wc -l) -eq 0 ]]; then
 fi
 
 # Install formulas
-brew install zsh-completions shellcheck eza fzf zoxide gh pyenv awscli tfenv az atmos checkov tfsec terraform-docs pre-commit yamlfmt yamllint yt-dlp ffmpeg helm htop jq yq lolcat figlet terraform-ls qemu progress bat k9s ansible
+brew install zsh-completions shellcheck eza fzf zoxide gh pyenv awscli tfenv az atmos checkov tfsec terraform-docs pre-commit yamlfmt yamllint yt-dlp ffmpeg helm btop htop jq yq lolcat figlet terraform-ls qemu progress bat k9s ansible
 
 # Install casks
 brew install --cask mac-mouse-fix rectangle shottr raycast iterm2 monitorcontrol bitwarden angry-ip-scanner hex-fiend appcleaner vlc
